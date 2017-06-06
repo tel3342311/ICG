c6639bdaaa6f5e8f064a9afb1e41a9c21c75654d
Allen Sun <tel3342311@gmail.com>
2017-06-06 18:25:47 +0800

    Add User Term Activity

---
+++ src/com/liteon/icampusguardian/UserTermActivity.java
@@ -0,0 +1,51 @@
+package com.liteon.icampusguardian;
+
+import android.content.Intent;
+import android.hardware.SensorEventListener;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+public class UserTermActivity extends AppCompatActivity implements OnClickListener {
+
+	
+	private TextView mAgree;
+	private TextView mQuit;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_userterm);
+		findViews();
+		setListener();
+	}
+	
+	private void findViews() {
+		mAgree = (TextView) findViewById(R.id.option_agree);
+		mQuit = (TextView) findViewById(R.id.option_quit);
+		
+	}
+	
+	private void setListener() {
+		mAgree.setOnClickListener(this);
+		mQuit.setOnClickListener(this);
+	}
+	
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.option_agree:
+			Intent intent = new Intent();
+			intent.setClass(this, LoginActivity.class);
+			startActivity(intent);
+			break;
+		case R.id.option_quit:
+			finish();
+			break;
+		}
+	}
+
+}

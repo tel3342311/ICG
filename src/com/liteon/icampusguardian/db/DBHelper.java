a49f06e84e23ef0ce612d361895bb86eb34ee886
Allen Sun <tel3342311@gmail.com>
2017-07-12 17:02:57 +0800

    Update DB helper

--- src/com/liteon/icampusguardian/db/DBHelper.java
+++ src/com/liteon/icampusguardian/db/DBHelper.java
@@ -8,6 +8,7 @@ import com.liteon.icampusguardian.db.AccountTable.AccountEntry;
 import com.liteon.icampusguardian.db.ChildLocationTable.ChildLocationEntry;
 import com.liteon.icampusguardian.db.ChildTable.ChildEntry;
 import com.liteon.icampusguardian.db.EventListTable.EventListEntry;
+import com.liteon.icampusguardian.util.JSONResponse.Return;
 import com.liteon.icampusguardian.util.JSONResponse.Student;
 
 import android.content.ContentValues;
@@ -21,81 +22,62 @@ import android.util.Log;
 public class DBHelper extends SQLiteOpenHelper {
 	private static DBHelper mInstance = null;
 	public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "data.db";
-    private static final String TEXT_TYPE = " TEXT";
-    private static final String INTEGER_TYPE = " INTEGER";
-    private static final String COMMA_SEP = ",";
-    
-    //ACCOUNT
-    public static final String SQL_QUERY_ALL_ACCOUNT_DATA = "SELECT * FROM " + AccountEntry.TABLE_NAME;
-    private static final String SQL_CREATE_ACCOUNT_TABLE =
-    	    "CREATE TABLE " + AccountEntry.TABLE_NAME + " (" +
-    	    AccountEntry.COLUMN_NAME_USER_NAME + TEXT_TYPE + " PRIMARY KEY" + COMMA_SEP +
-    	    AccountEntry.COLUMN_NAME_PASSWORD + TEXT_TYPE + COMMA_SEP +
-    	    AccountEntry.COLUMN_NAME_UUID + TEXT_TYPE + COMMA_SEP +
-    	    AccountEntry.COLUMN_NAME_ROLE_TYPE + TEXT_TYPE + COMMA_SEP +
-    	    AccountEntry.COLUMN_NAME_ACCOUNT_NAME + TEXT_TYPE + COMMA_SEP +
-    	    AccountEntry.COLUMN_NAME_GIVEN_NAME + TEXT_TYPE + COMMA_SEP +
-    	    AccountEntry.COLUMN_NAME_NICK_NAME + TEXT_TYPE + COMMA_SEP +
-    	    AccountEntry.COLUMN_NAME_GENDER + TEXT_TYPE + COMMA_SEP +
-    	    AccountEntry.COLUMN_NAME_HEIGHT + INTEGER_TYPE + COMMA_SEP +
-    	    AccountEntry.COLUMN_NAME_WEIGHT + INTEGER_TYPE + COMMA_SEP +
-    	    AccountEntry.COLUMN_NAME_DOB + TEXT_TYPE +  COMMA_SEP +
-    	    AccountEntry.COLUMN_NAME_TOKEN + TEXT_TYPE +
-    	    " )";
-    private static final String SQL_DELETE_ACCOUNT_TABLE =
-    	    "DROP TABLE IF EXISTS " + AccountEntry.TABLE_NAME;
-    
-    //Child data
-    public static final String SQL_QUERY_ALL_CHILDREN_DATA = "SELECT * FROM " + ChildEntry.TABLE_NAME;
-    private static final String SQL_CREATE_CHILDREN_TABLE =
-    	    "CREATE TABLE " + ChildEntry.TABLE_NAME + " ("  +
-    	    ChildEntry.COLUMN_NAME_UUID + TEXT_TYPE + " PRIMARY KEY" + COMMA_SEP +
-    	    ChildEntry.COLUMN_NAME_GIVEN_NAME + TEXT_TYPE + COMMA_SEP +
-    	    ChildEntry.COLUMN_NAME_NICK_NAME + TEXT_TYPE + COMMA_SEP +
-    	    ChildEntry.COLUMN_NAME_GENDER + TEXT_TYPE + COMMA_SEP +
-    	    ChildEntry.COLUMN_NAME_HEIGHT + INTEGER_TYPE + COMMA_SEP +
-    	    ChildEntry.COLUMN_NAME_WEIGHT + INTEGER_TYPE + COMMA_SEP +
-    	    ChildEntry.COLUMN_NAME_CLASS + TEXT_TYPE + COMMA_SEP +
-    	    ChildEntry.COLUMN_NAME_ROLL_NO + TEXT_TYPE + COMMA_SEP +
-    	    ChildEntry.COLUMN_NAME_STUDENT_ID + TEXT_TYPE + COMMA_SEP +
-    	    ChildEntry.COLUMN_NAME_DOB + TEXT_TYPE +
-    	    " )";
-    private static final String SQL_DELETE_CHILD_TABLE =
-    	    "DROP TABLE IF EXISTS " + ChildEntry.TABLE_NAME;
-    
-    //Event list data
-    public static final String SQL_QUERY_ALL_EVENT_DATA = "SELECT * FROM " + EventListEntry.TABLE_NAME;
-    private static final String SQL_CREATE_EVENT_TABLE =
-    	    "CREATE TABLE " + EventListEntry.TABLE_NAME + " (" +
-    	    EventListEntry.COLUMN_NAME_UUID + TEXT_TYPE + " PRIMARY KEY"+ COMMA_SEP +
-    	    EventListEntry.COLUMN_NAME_EVENT_ID + TEXT_TYPE + COMMA_SEP +
-    	    EventListEntry.COLUMN_NAME_EVENT_SUBSCRIBE + TEXT_TYPE +
-    	    " )";
-    private static final String SQL_DELETE_EVENT_TABLE =
-    	    "DROP TABLE IF EXISTS " + EventListEntry.TABLE_NAME;
-    
-    public static DBHelper getInstance(Context ctx) {
-        if (mInstance == null) {
-            mInstance = new DBHelper(ctx.getApplicationContext());
-        }
-        return mInstance;
-    }
-    //childe location data
-    public static final String SQL_QUERY_CHILD_LOCATION_DATA = "SELECT * FROM " + ChildLocationEntry.TABLE_NAME;
-    private static final String SQL_CREATE_CHILD_LOCAITON_TABLE =
-    	    "CREATE TABLE " + ChildLocationEntry.TABLE_NAME + " (" +
-    	    ChildLocationEntry.COLUMN_NAME_UUID + TEXT_TYPE + " PRIMARY KEY"+ COMMA_SEP +
-    	    ChildLocationEntry.COLUMN_NAME_LATITUDE + TEXT_TYPE + COMMA_SEP +
-    	    ChildLocationEntry.COLUMN_NAME_LONGITUDE + TEXT_TYPE +
-    	    " )";
-    private static final String SQL_DELETE_CHILD_LOCATION_TABLE =
-    	    "DROP TABLE IF EXISTS " + EventListEntry.TABLE_NAME;
-    
-    
-    private DBHelper(Context context) {
-    	super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
+	public static final String DATABASE_NAME = "data.db";
+	private static final String TEXT_TYPE = " TEXT";
+	private static final String INTEGER_TYPE = " INTEGER";
+	private static final String COMMA_SEP = ",";
+
+	// ACCOUNT
+	public static final String SQL_QUERY_ALL_ACCOUNT_DATA = "SELECT * FROM " + AccountEntry.TABLE_NAME;
+	private static final String SQL_CREATE_ACCOUNT_TABLE = "CREATE TABLE " + AccountEntry.TABLE_NAME + " ("
+			+ AccountEntry.COLUMN_NAME_USER_NAME + TEXT_TYPE + " PRIMARY KEY" + COMMA_SEP
+			+ AccountEntry.COLUMN_NAME_PASSWORD + TEXT_TYPE + COMMA_SEP + AccountEntry.COLUMN_NAME_UUID + TEXT_TYPE
+			+ COMMA_SEP + AccountEntry.COLUMN_NAME_ROLE_TYPE + TEXT_TYPE + COMMA_SEP
+			+ AccountEntry.COLUMN_NAME_ACCOUNT_NAME + TEXT_TYPE + COMMA_SEP + AccountEntry.COLUMN_NAME_GIVEN_NAME
+			+ TEXT_TYPE + COMMA_SEP + AccountEntry.COLUMN_NAME_NICK_NAME + TEXT_TYPE + COMMA_SEP
+			+ AccountEntry.COLUMN_NAME_GENDER + TEXT_TYPE + COMMA_SEP + AccountEntry.COLUMN_NAME_HEIGHT + INTEGER_TYPE
+			+ COMMA_SEP + AccountEntry.COLUMN_NAME_WEIGHT + INTEGER_TYPE + COMMA_SEP + AccountEntry.COLUMN_NAME_DOB
+			+ TEXT_TYPE + COMMA_SEP + AccountEntry.COLUMN_NAME_TOKEN + TEXT_TYPE + " )";
+	private static final String SQL_DELETE_ACCOUNT_TABLE = "DROP TABLE IF EXISTS " + AccountEntry.TABLE_NAME;
+
+	// Child data
+	public static final String SQL_QUERY_ALL_CHILDREN_DATA = "SELECT * FROM " + ChildEntry.TABLE_NAME;
+	private static final String SQL_CREATE_CHILDREN_TABLE = "CREATE TABLE " + ChildEntry.TABLE_NAME + " ("
+			+ ChildEntry.COLUMN_NAME_UUID + TEXT_TYPE + " PRIMARY KEY" + COMMA_SEP + ChildEntry.COLUMN_NAME_GIVEN_NAME
+			+ TEXT_TYPE + COMMA_SEP + ChildEntry.COLUMN_NAME_NICK_NAME + TEXT_TYPE + COMMA_SEP
+			+ ChildEntry.COLUMN_NAME_GENDER + TEXT_TYPE + COMMA_SEP + ChildEntry.COLUMN_NAME_HEIGHT + INTEGER_TYPE
+			+ COMMA_SEP + ChildEntry.COLUMN_NAME_WEIGHT + INTEGER_TYPE + COMMA_SEP + ChildEntry.COLUMN_NAME_CLASS
+			+ TEXT_TYPE + COMMA_SEP + ChildEntry.COLUMN_NAME_ROLL_NO + TEXT_TYPE + COMMA_SEP
+			+ ChildEntry.COLUMN_NAME_STUDENT_ID + TEXT_TYPE + COMMA_SEP + ChildEntry.COLUMN_NAME_DOB + TEXT_TYPE + " )";
+	private static final String SQL_DELETE_CHILD_TABLE = "DROP TABLE IF EXISTS " + ChildEntry.TABLE_NAME;
+
+	// Event list data
+	public static final String SQL_QUERY_ALL_EVENT_DATA = "SELECT * FROM " + EventListEntry.TABLE_NAME;
+	private static final String SQL_CREATE_EVENT_TABLE = "CREATE TABLE " + EventListEntry.TABLE_NAME + " ("
+			+ EventListEntry.COLUMN_NAME_UUID + TEXT_TYPE + " PRIMARY KEY" + COMMA_SEP
+			+ EventListEntry.COLUMN_NAME_EVENT_ID + TEXT_TYPE + COMMA_SEP + EventListEntry.COLUMN_NAME_EVENT_SUBSCRIBE
+			+ TEXT_TYPE + " )";
+	private static final String SQL_DELETE_EVENT_TABLE = "DROP TABLE IF EXISTS " + EventListEntry.TABLE_NAME;
+
+	public static DBHelper getInstance(Context ctx) {
+		if (mInstance == null) {
+			mInstance = new DBHelper(ctx.getApplicationContext());
+		}
+		return mInstance;
+	}
+
+	// childe location data
+	public static final String SQL_QUERY_CHILD_LOCATION_DATA = "SELECT * FROM " + ChildLocationEntry.TABLE_NAME;
+	private static final String SQL_CREATE_CHILD_LOCAITON_TABLE = "CREATE TABLE " + ChildLocationEntry.TABLE_NAME + " ("
+			+ ChildLocationEntry.COLUMN_NAME_UUID + TEXT_TYPE + " PRIMARY KEY" + COMMA_SEP
+			+ ChildLocationEntry.COLUMN_NAME_LATITUDE + TEXT_TYPE + COMMA_SEP + ChildLocationEntry.COLUMN_NAME_LONGITUDE
+			+ TEXT_TYPE + " )";
+	private static final String SQL_DELETE_CHILD_LOCATION_TABLE = "DROP TABLE IF EXISTS " + EventListEntry.TABLE_NAME;
+
+	private DBHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(SQL_CREATE_ACCOUNT_TABLE);
@@ -112,17 +94,29 @@ public class DBHelper extends SQLiteOpenHelper {
 		db.execSQL(SQL_DELETE_CHILD_LOCATION_TABLE);
 		onCreate(db);
 	}
+
 	public String getAccountToken(SQLiteDatabase db, String name) {
-	    Cursor c = db.query(AccountEntry.TABLE_NAME, new String[]{AccountEntry.COLUMN_NAME_TOKEN}, "username =?",new String[]{name},null,null,null,null);
-	    if (c.moveToFirst()) //if the row exist then return the id
-	        return c.getString(c.getColumnIndex(AccountEntry.COLUMN_NAME_TOKEN));
-	    return "";
+		Cursor c = db.query(AccountEntry.TABLE_NAME, new String[] { AccountEntry.COLUMN_NAME_TOKEN }, "username =?",
+				new String[] { name }, null, null, null, null);
+		if (c.moveToFirst()) // if the row exist then return the id
+			return c.getString(c.getColumnIndex(AccountEntry.COLUMN_NAME_TOKEN));
+		return "";
 	}
-	
+
 	public long insertAccount(SQLiteDatabase db, ContentValues value) {
-		return db.insert(AccountEntry.TABLE_NAME, "", value);
+
+		long ret = db.insert(AccountEntry.TABLE_NAME, "", value);
+		db.close();
+		return ret;
+
 	}
-	
+
+	public long deleteAccount(SQLiteDatabase db) {
+		long ret = db.delete(AccountEntry.TABLE_NAME, null, null);
+		db.close();
+		return ret;
+	}
+
 	public void insertChildList(SQLiteDatabase db, List<Student> childList) {
 		for (Student item : childList) {
 			ContentValues cv = new ContentValues();
@@ -137,7 +131,7 @@ public class DBHelper extends SQLiteOpenHelper {
 			cv.put(ChildEntry.COLUMN_NAME_CLASS, item.get_class());
 			cv.put(ChildEntry.COLUMN_NAME_STUDENT_ID, item.getStudent_id());
 			if (isChildExist(db, item.getUuid())) {
-				db.update(ChildEntry.TABLE_NAME, cv, "uuid=?", new String[] {item.getUuid()});
+				db.update(ChildEntry.TABLE_NAME, cv, "uuid=?", new String[] { item.getUuid() });
 			} else {
 				long ret = db.insert(ChildEntry.TABLE_NAME, null, cv);
 				Log.d(DATABASE_NAME, "insert RET is " + ret);
@@ -145,24 +139,24 @@ public class DBHelper extends SQLiteOpenHelper {
 		}
 		db.close();
 	}
-	
+
 	public boolean isChildExist(SQLiteDatabase db, String uuid) {
 		if (TextUtils.isEmpty(uuid)) {
 			return false;
 		}
-		Cursor c = db.query(ChildEntry.TABLE_NAME, null, "uuid =?",new String[]{uuid},null,null,null,null);
-	    if (c.moveToFirst()) { //if the row exist then return the id
-	    	c.close();
-	        return true;
-	    }
-	    c.close();
-	    return false;
+		Cursor c = db.query(ChildEntry.TABLE_NAME, null, "uuid =?", new String[] { uuid }, null, null, null, null);
+		if (c.moveToFirst()) { // if the row exist then return the id
+			c.close();
+			return true;
+		}
+		c.close();
+		return false;
 	}
-	
+
 	public List<Student> queryChildList(SQLiteDatabase db) {
 		List<Student> list = new ArrayList<>();
 		Cursor cursor = db.rawQuery(SQL_QUERY_ALL_CHILDREN_DATA, null);
-		if (cursor.getCount() > 0 ) {
+		if (cursor.getCount() > 0) {
 			cursor.moveToFirst();
 			do {
 				Student item = new Student();
@@ -174,14 +168,13 @@ public class DBHelper extends SQLiteOpenHelper {
 				item.setHeight(cursor.getInt(cursor.getColumnIndex(ChildEntry.COLUMN_NAME_HEIGHT)));
 				item.setWeight(cursor.getInt(cursor.getColumnIndex(ChildEntry.COLUMN_NAME_WEIGHT)));
 				item.setRoll_no(cursor.getString(cursor.getColumnIndex(ChildEntry.COLUMN_NAME_ROLL_NO)));
-				item.set_class(cursor.getString(cursor.getColumnIndex(ChildEntry.COLUMN_NAME_CLASS)));   
+				item.set_class(cursor.getString(cursor.getColumnIndex(ChildEntry.COLUMN_NAME_CLASS)));
 				item.setStudent_id(cursor.getString(cursor.getColumnIndex(ChildEntry.COLUMN_NAME_STUDENT_ID)));
 				list.add(item);
-			} while(cursor.moveToNext());
+			} while (cursor.moveToNext());
 			cursor.close();
 			db.close();
 		}
 		return list;
 	}
 }
-

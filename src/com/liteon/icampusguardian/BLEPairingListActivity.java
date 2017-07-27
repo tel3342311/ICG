98a1f87dd923befa366e577da0019903e3720f60
Allen Sun <tel3342311@gmail.com>
2017-07-27 16:08:33 +0800

    Add permission of read file

--- src/com/liteon/icampusguardian/BLEPairingListActivity.java
+++ src/com/liteon/icampusguardian/BLEPairingListActivity.java
@@ -1,15 +1,21 @@
 package com.liteon.icampusguardian;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.w3c.dom.Text;
-
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
 import com.liteon.icampusguardian.util.BLEItem;
 import com.liteon.icampusguardian.util.BLEItemAdapter;
-import com.liteon.icampusguardian.util.ConfirmDeleteDialog;
-import com.liteon.icampusguardian.util.CustomDialog;
 import com.liteon.icampusguardian.util.BLEItemAdapter.ViewHolder.IBLEItemClickListener;
+import com.liteon.icampusguardian.util.CustomDialog;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -30,8 +36,9 @@ import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
-import android.support.v7.app.AlertDialog;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -60,22 +67,70 @@ public class BLEPairingListActivity extends AppCompatActivity implements IBLEIte
     private List<ScanFilter> filters;
     private BluetoothGatt mGatt;
     private static int VERSION_CODES = 21;
-    private final static int PERMISSION_REQUEST_COARSE_LOCATION = 3;
+    private final static int PERMISSION_REQUEST = 3;
+
+    //work around for uuid
+    private UUIDList mUUIDList;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		askPermisson();
+		//read uuid file
+		File downloadDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() + "/" +"UUID.txt");
+
+		getFilesFromDir(downloadDir);	
 		setContentView(R.layout.activity_ble_pairing_list);
 		findViews();
 		setListener();
 		initRecycleView();
 		initBleComponent();
-		askPermisson();
+
+		
+
+	}
+	
+	public static boolean hasPermissions(Context context, String... permissions) {
+	    if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+	        for (String permission : permissions) {
+	            if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+	                return false;
+	            }
+	        }
+	    }
+	    return true;
+	}
+	
+	public void getFilesFromDir(File currentFile) {
+
+		try {
+			FileInputStream iStream = new FileInputStream(currentFile);
+			mUUIDList = (UUIDList) getFakeUUID(iStream, UUIDList.class);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private Object getFakeUUID(InputStream is, Class<?> class_type) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+			while ((line = br.readLine()) != null) {
+			    sb.append(line+"\n");
+			}
+	        br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        return new Gson().fromJson(sb.toString(), class_type);
 	}
 	
 	private void askPermisson() {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 			if (this.checkSelfPermission(android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-				requestPermissions(new String[] {android.Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+				requestPermissions(new String[] {android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST);
 			}
 		}
 	}
@@ -83,7 +138,7 @@ public class BLEPairingListActivity extends AppCompatActivity implements IBLEIte
 	@Override
 	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
 		switch(requestCode) {
-		case PERMISSION_REQUEST_COARSE_LOCATION:
+		case PERMISSION_REQUEST:
 			if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 				Log.d(TAG, "coarse location permission granted");
 			} else {
@@ -93,8 +148,30 @@ public class BLEPairingListActivity extends AppCompatActivity implements IBLEIte
         		dialog.setBtnText("好");
         		dialog.show(getSupportFragmentManager(), "dialog_fragment");
 			}
+			
+			if (grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+				Log.d(TAG, "coarse location permission granted");
+			} else {
+				CustomDialog dialog = new CustomDialog();
+        		dialog.setTitle("應用程式要求權限以繼續");
+        		dialog.setIcon(0);
+        		dialog.setBtnText("好");
+        		dialog.show(getSupportFragmentManager(), "dialog_fragment");
+			}
+			
+			if (grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+				Log.d(TAG, "coarse location permission granted");
+			} else {
+				CustomDialog dialog = new CustomDialog();
+        		dialog.setTitle("應用程式要求權限以繼續");
+        		dialog.setIcon(0);
+        		dialog.setBtnText("好");
+        		dialog.show(getSupportFragmentManager(), "dialog_fragment");
+			}
+			break;
 		}
 	}
+	
 	private void initBleComponent() {
 		mHandler = new Handler();
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
@@ -118,11 +195,20 @@ public class BLEPairingListActivity extends AppCompatActivity implements IBLEIte
 	
 	private void setupData(){
 		mDataSet = new ArrayList<>();
-		BLEItem item = new BLEItem();
-		item.setId("0000-0000-0000-0000");
-		item.setName("iCampus Guardian");
-		item.setValue("Not Connected");
-		mDataSet.add(item);
+		if (mUUIDList != null) {
+			for (Device device : mUUIDList.getDevices()) {
+				BLEItem item = new BLEItem();
+				item.setId(device.getUuid());
+				item.setName(device.getName());
+				mDataSet.add(item);
+			}
+		} else {
+			BLEItem item = new BLEItem();
+			item.setId("0000-0000-0000-0000");
+			item.setName("iCampus Guardian");
+			item.setValue("Not Connected");
+			mDataSet.add(item);
+		}
 	}
 
 	@Override
@@ -343,5 +429,56 @@ public class BLEPairingListActivity extends AppCompatActivity implements IBLEIte
 		Intent intent = new Intent();
 		intent.setClass(this, BLEPinCodeInputActivity.class);
 		startActivity(intent);
+	}
+	
+	class UUIDList {
+		
+		@SerializedName("devices")
+		private Device[] devices;
+
+		/**
+		 * @return the devices
+		 */
+		public Device[] getDevices() {
+			return devices;
+		}
+
+		/**
+		 * @param devices the devices to set
+		 */
+		public void setDevices(Device[] devices) {
+			this.devices = devices;
+		}
+	}
+	class Device {
+		@SerializedName("name")
+		private String name;
+		@SerializedName("uuid")
+		private String uuid;
+		/**
+		 * @return the name
+		 */
+		public String getName() {
+			return name;
+		}
+		/**
+		 * @param name the name to set
+		 */
+		public void setName(String name) {
+			this.name = name;
+		}
+		/**
+		 * @return the uuid
+		 */
+		public String getUuid() {
+			return uuid;
+		}
+		/**
+		 * @param uuid the uuid to set
+		 */
+		public void setUuid(String uuid) {
+			this.uuid = uuid;
+		}
+		
 	}
 }
